ba3a4decabb5d3fbda701c8da8fd566f787009f5
riahimedyassin <riahimohamedyassin@charguia.r-iset.tn>
2023-11-15 17:08:37 +0100

    Fixed a high risk stupid auth bug

--- Backend/middlewares/auth/admin.auth.js
+++ Backend/middlewares/auth/admin.auth.js
@@ -1,5 +1,6 @@
 const { createError } = require("../../errors/customError");
 const { verfiyToken } = require("../../utils/token/verifyToken");
+const Admin = require("../../models/admin.model");
 
 const requireAdminAuth = async (req, res, next) => {
   try {
@@ -9,6 +10,8 @@ const requireAdminAuth = async (req, res, next) => {
     const token = authorization.split(" ")[1];
     const verfied = await verfiyToken(token);
     if (!verfied) return next(createError("Unauth", 403));
+    const member = await Admin.findOne({ _id: verfied.id });
+    if (!member) return next(createError("Unauthorized", 403));
     req.user = verfied.id;
     next();
   } catch (error) {
